0a997847d4ec31a902973dafd3abcc77dab67df0
Sahil Shinde <sahilshinde2402@gmail.com>
2026-02-18 20:17:42 +0530

    Add animated background to App component
    Added an animated background with gradient orbs and a grid pattern.

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,15 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
+      {/* Animated Background */}
+      <div className="background-gradient">
+        <div className="gradient-orb orb-1"></div>
+        <div className="gradient-orb orb-2"></div>
+        <div className="gradient-orb orb-3"></div>
+        <div className="gradient-orb orb-4"></div>
+      </div>
+      <div className="grid-pattern"></div>
+      
       <Navbar />
       <main>
         <Hero />
